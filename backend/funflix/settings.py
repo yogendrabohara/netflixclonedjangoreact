d4cb0a8e3cff571c27a5165a0314b4a28a959ab1
yogendra bohara <sabiyogi19@gmail.com>
2021-04-21 10:26:19 -0700

    django-heroku added

--- backend/funflix/settings.py
+++ backend/funflix/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -154,3 +155,6 @@ REST_FRAMEWORK = {
     ),
 
 }
+
+STATIC_ROOT = 'static'
+django_heroku.settings(locals())
