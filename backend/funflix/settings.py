a3ca469071c633b442de542e461aac737d97d79e
yogendra bohara <sabiyogi19@gmail.com>
2021-04-23 13:49:30 -0700

    movies categories were tallied

--- backend/funflix/settings.py
+++ backend/funflix/settings.py
@@ -147,8 +147,8 @@ MEDIA_URL = '/media/'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication'
